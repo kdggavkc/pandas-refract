088a557363f0146739ec22ae6513e15bc2339ce3
kdggavkc <Mivr6345G>
2018-07-14 11:45:04 -0500

    add disperse function and upgrade

--- pandas_refract.py
+++ pandas_refract.py
@@ -2,7 +2,7 @@ import numpy as np
 
 
 __title__ = "pandas-refract"
-__version__ = "1.2"
+__version__ = "1.2.0"
 __author__ = "Nicholas Lawrence"
 __license__ = "MIT"
 __copyright__ = "Copyright 2018-2019 Nicholas Lawrence"
